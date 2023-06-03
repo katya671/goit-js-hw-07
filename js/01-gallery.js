ba8907ed5d9f60913736a8724f5e27ff545ab93a
Толочко Катерина <bantiktokinpakistan@gmail.com>
2023-06-03 22:33:37 +0300

    Done homework 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+            </a>
+        </li>`
+    }).join('');
+}
+
+gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+const onClick = (event) => {
+    event.preventDefault();
+    if (event.target.tagName !== "IMG") {
+        return;
+    }
+    
+    const imageURL = event.target.dataset.source;
+
+    const modal = basicLightbox.create(`<img src="${imageURL}">`);
+
+    modal.element().querySelector('img').src = imageURL;
+    
+    modal.show();
+}
+
+gallery.addEventListener('click', onClick);
