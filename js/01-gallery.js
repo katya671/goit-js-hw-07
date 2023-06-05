f0eb39886ce4fd4e033a7342ca0fca2232af28a4
Толочко Катерина <bantiktokinpakistan@gmail.com>
2023-06-05 17:08:03 +0300

    Add options for basicLightBox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,40 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 
 function createGalleryMarkup(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
-        return `<li class="gallery__item">
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
             <a class="gallery__link" href="${original}">
                 <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
             </a>
-        </li>`
-    }).join('');
+        </li>`;
+    })
+    .join("");
 }
 
-gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+gallery.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
 
-function onClick (event) {
-    event.preventDefault();
-    if (event.target.tagName !== "IMG") {
-        return;
+function onClick(event) {
+  event.preventDefault();
+  if (event.target.tagName !== "IMG") {
+    return;
+  }
+
+  const imageURL = event.target.dataset.source;
+
+  const options = {
+    onShow: () => {
+      document.addEventListener("keydown", onKeyDown);
+      console.log("listening now");
+    },
+    onClose: () => {
+      document.removeEventListener("keydown", onKeyDown);
+      console.log("remove listener");
+    },
+  };
+
+  const modal = basicLightbox.create(`<img src="${imageURL}">`, options);
+
+  modal.element().querySelector("img").src = imageURL;
+
+  function onKeyDown(event) {
+    if (event.key === "Escape") {
+      modal.close();
     }
+  }
 
-    const imageURL = event.target.dataset.source;
-
-    const modal = basicLightbox.create(`<img src="${imageURL}">`);
-
-    modal.element().querySelector('img').src = imageURL;
-    
-    modal.show(() => { document.addEventListener('keydown', onKeyDown) });
-
-    function onKeyDown(event) {
-        if (event.key === "Escape") {
-            modal.close();
-            document.removeEventListener('keydown', onKeyDown);
-        }
-    }
+  modal.show();
 }
 
-gallery.addEventListener('click', onClick);
+gallery.addEventListener("click", onClick);
